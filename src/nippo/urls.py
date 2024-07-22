788ec633ba4fd240b266546cb23d4a011373c4e8
motoaki <taobody@gmail.com>
2024-07-22 15:07:47 +0900

    2024-7-21 1-5～2-6

--- src/nippo/urls.py
+++ src/nippo/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from .views import nippoListView, nippoDetailView, nippoCreateView
 
 urlpatterns = [
-    
+    path("", nippoListView, name="nippo-list"),
+    path("detail/<int:number>/", nippoDetailView, name="nippo-detail"),
+    path("create/", nippoCreateView, name="nippo-create"),
 ]
 
--- src/nippo/views.py
+++ src/nippo/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from random import randint
 
-# Create your views here.
+def nippoListView(request):
+    return render(request, "nippo/nippo-list.html")
+
+def nippoDetailView(request, number):
+    template_name = "nippo/nippo-detail.html"
+    random_int = randint(1, 10)
+    ctx = {
+        "random_number" : random_int,
+        "number" : number,
+    }
+    return render(request, template_name, ctx)
+
+def nippoCreateView(request):
+    template_name = "nippo/nippo-form.html"
+
+    if request.POST:
+        title = request.POST.get("title")
+        content = request.POST.get("content")
+    # print(title)
+    # print(content)
+
+    return render(request, template_name)
